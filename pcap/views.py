a2a782c6a886719e3986789c873f961cd8e2035e
Carlos Henriquez <carloschulo@gmail.com>
2018-02-10 17:51:53 +0800

    adding pcap template

--- pcap/views.py
+++ pcap/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Upload PCAP.")
+    # return HttpResponse("Upload PCAP.")
+    return render(request, 'pcap/index.html')
