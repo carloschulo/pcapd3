b542d2b85a5e9e0348023b2f8a1458454a3c500a
Carlos Henriquez <carloschulo@gmail.com>
2018-02-13 03:30:42 +0800

    added form createView func

--- pcap/models.py
+++ pcap/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
+
 class Pcap(models.Model):
     pcap_file = models.FileField()
+    # print(pcap_file)
+
+    def __str__(self):
+        return self.pcap_file.name
+
+    def get_absolute_url(self):
+        return reverse('index')
--- pcap/views.py
+++ pcap/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from django.views import generic
+from django.views.generic.edit import CreateView
+from .models import Pcap
 
 from django.http import HttpResponse
 
 
-def index(request):
-    # return HttpResponse("Upload PCAP.")
-    return render(request, 'pcap/index.html')
+class IndexView(generic.ListView, ):
+    template_name = 'pcap/index.html'
+    context_object_name = 'pcaps'
+
+    def get_queryset(self):
+        return Pcap.objects.all()
+
+
+class AddPcap(CreateView):
+    model = Pcap
+    fields = ['pcap_file']
+
