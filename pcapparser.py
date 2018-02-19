b29fb7d534f5d3b0d39284b3fa595d7527d9b595
Carlos Henriquez <carloschulo@gmail.com>
2018-02-19 13:25:15 +0700

    updating logic

--- pcapparser.py
+++ pcapparser.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
 
-import os
+import os, subprocess, csv, operator
 
 # argu = "tshark -r sample.pcap -T fields -E separator=, -E quote=d -e tcp.dstport -e ip.addr -e tcp.stream | sort | uniq | sort > output.csv"
 #
 # os.system(argu)
-returns = os.system("tshark -r sample.pcap -T fields -E separator=, -E quote=d -e tcp.dstport -e ip.addr -e tcp.stream | sort | uniq | sort")
+# returns = os.system("tshark -r sample.pcap -T fields -E separator=, -E quote=d -e tcp.dstport -e ip.addr -e tcp.stream | sort | uniq | sort")
 
 # print()
+#
+# inpu = "tshark -r sample.pcap -T fields -e frame.number -e tcp.dstport -e ip.addr -e tcp.stream -l"
+# p = subprocess.Popen(inpu, stdout=subprocess.PIPE, shell=True)
+
+# print(p.stdout.readline())
+#
+# for frame in iter(p.stdout.readline):
+#     print(frame)
+
+# tshark = "tshark -r sample.pcap -T fields -e frame.number -e tcp.dstport -e ip.addr -e tcp.stream > out.txt"
+# os.system(tshark)
+# tcsv = "tshark -r sample.pcap -T fields -e tcp.stream  -e frame.number -e tcp.dstport -e ip.addr -E separator=, > out.csv"
+# os.system(tcsv)
+#
+# with open('out.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     sort = sorted(csv_reader, key=lambda x: int(x[0]))
+#     for line in sort:
+#         print(line)
+
+filepath = "tezt.txt"
+# file = open(filepath, 'r')
+# lines = file.readlines()
+# file.close()
+
+with open(filepath) as f:
+    for line in f:
+        print(line.strip())
+
+
