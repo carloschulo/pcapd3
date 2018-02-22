74dfaa169ad8bb43ab7c372bdf61d2ffc9656859
Carlos Henriquez <carloschulo@gmail.com>
2018-02-23 01:00:54 +0700

    changin to func based view for the upload

--- pcap/urls.py
+++ pcap/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('upload/', views.AddPcap.as_view(), name='upload')
+    path('upload/', views.AddPcap.as_view(), name='upload'),
+    path('uploadz/', views.upload, name='uploadz')
+
 ]
--- pcap/views.py
+++ pcap/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic.edit import CreateView
 from .models import Pcap
+from .forms import UploadForm
+from django.urls import reverse
+
 
 from django.http import HttpResponse
 
+def upload(request):
+    form = UploadForm(request.POST or None, request.FILES or None)
+    uploadpcap = UploadForm()
+    if form.is_valid():
+        form.save()
+        file = request.FILES['pcap_file']
+        filename = str(file)
+        print(filename)
+        return HttpResponse('Success!')
+    return render(request, 'pcap/pcap_form.html', {'form': uploadpcap})
 
-class IndexView(generic.ListView, ):
+class IndexView(generic.ListView):
     template_name = 'pcap/index.html'
     context_object_name = 'pcaps'
 
@@ -17,4 +30,13 @@ class IndexView(generic.ListView, ):
 class AddPcap(CreateView):
     model = Pcap
     fields = ['pcap_file']
+    # template_name = 'pcap/pcap_form.html'
+    # success_url = 'pcap/'
+    #
+    # def post(self, request, *args, **kwargs):
+    #     files = request.FILES.get('pcapupload')
+    #     print('files', files)
+    #     return render(request, 'pcap/success.html')
+
+
 
