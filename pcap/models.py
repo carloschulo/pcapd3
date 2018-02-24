41b79c39a06ca5da74841a6fdac280e8b9940284
Carlos Henriquez <carloschulo@gmail.com>
2018-02-25 03:24:05 +0700

    clean up

--- pcap/models.py
+++ pcap/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
 
 class Pcap(models.Model):
     pcap_file = models.FileField()
-    # print(pcap_file)
 
     def __str__(self):
         return self.pcap_file.name
