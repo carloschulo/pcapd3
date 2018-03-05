dd428f435a0c8109147ccabe57ec1a7ad9fcf314
Carlos Henriquez <carloschulo@gmail.com>
2018-03-05 17:57:25 +0530

    setup redirect for d3 visuals

--- pcap/urls.py
+++ pcap/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # path('upload/', views.AddPcap.as_view(), name='upload'),
-    path('upload/', views.upload, name='upload')
-
+    path('upload/', views.upload, name='upload'),
+    path('visualize/', views.visualizepcap, name='visualizer'),
 ]
--- pcap/views.py
+++ pcap/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.views.generic.edit import CreateView
 from .models import Pcap
 from .forms import UploadForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from.parser import tsharkpcap
 
 
@@ -16,8 +16,11 @@ def upload(request):
         filename = str(file)
         # handle uploaded file
         tsharkpcap(file, filename)
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/visualize')
     return render(request, 'pcap/pcap_form.html', {'form': uploadpcap})
+
+def visualizepcap(request):
+    return HttpResponse('Hello there sweety')
 
 class IndexView(generic.ListView):
     template_name = 'pcap/index.html'
@@ -26,7 +29,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Pcap.objects.all()
 
-
+# not using this class atm
 class AddPcap(CreateView):
     model = Pcap
     fields = ['pcap_file']
