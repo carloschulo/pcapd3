3d63c91fd9a060f5fe0216b5caa9cd515cc836c3
Carlos Henriquez <carloschulo@gmail.com>
2018-03-09 03:11:17 +0530

    updated for testing

--- pcapparser.py
+++ pcapparser.py
@@ -2,68 +2,6 @@
 
 import os, subprocess, csv, operator, re, pandas as pd
 
-# argu = "tshark -r sample.pcap -T fields -E separator=, -E quote=d -e tcp.dstport -e ip.addr -e tcp.stream | sort | uniq | sort > output.csv"
-#
-# os.system(argu)
-# returns = os.system("tshark -r sample.pcap -T fields -E separator=, -E quote=d -e tcp.dstport -e ip.addr -e tcp.stream | sort | uniq | sort")
-
-# print()
-#
-# inpu = "tshark -r sample.pcap -T fields -e frame.number -e tcp.dstport -e ip.addr -e tcp.stream -l"
-# p = subprocess.Popen(inpu, stdout=subprocess.PIPE, shell=True)
-
-# print(p.stdout.readline())
-#
-# for frame in iter(p.stdout.readline):
-#     print(frame)
-
-# tshark = "tshark -r sample.pcap -T fields -e frame.number -e tcp.dstport -e ip.addr -e tcp.stream > out.txt"
-# os.system(tshark)
-# tcsv = "tshark -r sample.pcap -T fields -e tcp.stream  -e frame.number -e tcp.dstport -e ip.addr -E separator=, > out.csv"
-# os.system(tcsv)
-#
-# with open('out.csv', 'r') as csv_file:
-#     csv_reader = csv.reader(csv_file)
-#     sort = sorted(csv_reader, key=lambda x: int(x[0]))
-#     for line in sort:
-#         print(line)
-
-
-x = []
-dic = {}
-newDic = {}
-
-def handlelist(index, input):
-    if input[0] != '':
-        # if index ==
-        x.append(input)
-        # dic[index] = input
-
-        # print(index, input)
-    else:
-        return
-
-
-def handlefile():
-    filepath = "tezt.txt"
-    with open(filepath) as f:
-        for index, line in enumerate(f):
-            packet = line.splitlines()
-            newpacket = re.split(r'\t+', packet[0].rstrip('\t'))
-            handlelist(index, newpacket)
-
-
-# def splitlist(ls):
-#     listlen = ls.__len__()
-#     for elem in ls:
-#         if elem[0] ==
-#     print(ls)
-
-
-# handlefile()
-# splitlist(x)
-# print(x)
-
 with open('udp-sankey.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     # remove arrows
@@ -80,6 +18,7 @@ with open('udp-sankey.txt', 'r') as in_file:
 df = pd.read_csv('udp-sankey.csv')
 length = len(df.index)
 # print('length is ' + str(length))
+df.sort_values(["Relative Start"], inplace=True, ascending=True)
 df['Total Packet'] = round(df['Total Packet'] * .1, 5)
 df.drop(['Packet B to A Frames', 'Packet B to A Bytes', 'Packet A to B Frames', 'Packet A to B Bytes', 'Total Bytes', 'Relative Start', 'Duration'], axis=1, inplace=True)
 df.rename(columns={'Address A': 'source', 'Address B': 'target', 'Total Packet': 'value'}, inplace=True)
